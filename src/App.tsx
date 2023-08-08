cfb12c9dfebf422af460962f6d962b1bae10ec68
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-08-08 14:32:38 -0700

    add a grid for the Password entry inputs.

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import "./App.css";
 
 const SignInForm = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded w-2/3">
-        <h2 className="flex justify-center text-2xl font-bold mb-4">
-          Create your Weeded Account
+    <div className="landing-page flex justify-center items-center h-screen">
+      <div className="p-8 rounded w-2/3">
+        <h2 className="common-text flex justify-center text-2xl font-bold mb-4">
+          Create your Weeded account
         </h2>
         <form>
           <div className="mb-10">
             <label
               htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
+              className="common-text block text-sm font-medium text-gray-700"
             >
               Email Address
             </label>
@@ -25,34 +25,40 @@ const SignInForm = () => {
             />
           </div>
 
-          <div className="side-by-side mb-5">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="mt-1 p-2 border rounded w-1/2"
-              placeholder="Enter your password"
-            />
+          <div className="grid gap-6 md:grid-cols-2">
 
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="mt-1 p-2 border rounded w-1/2"
-              placeholder="Re-enter your password"
-            />
+            <div className="side-by-side mb-5">
+              <label
+                htmlFor="password"
+                className="common-text block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="mt-1 p-2 border rounded w-full"
+                placeholder="Enter your password"
+              />
+            </div>
+
+            <div className="side-by-side mb-5">
+              <label
+                htmlFor="password"
+                className="common-text block text-sm font-medium text-gray-700"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="mt-1 p-2 border rounded w-full"
+                placeholder="Re-enter your password"
+              />
+            </div>
           </div>
 
+          {/* change buttons to be separate divs for grid to function properly */}
           <div className="buttons-side-by-side ">
             <button
               type="submit"
